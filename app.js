926a10a716e47a40896e5e2ebfd705594c2722b8
zero-stealth <penguincipher@gmail.com>
2024-02-03 00:46:30 +0300

    stable

--- app.js
+++ app.js
@@ -7,22 +7,20 @@ const morgan = require("morgan");
 const helmet = require('helmet');
 const MongoStore = require('connect-mongo');
 const bodyParser = require("body-parser");
+const multer = require('multer');
 const { connectDB } = require("./config/db");
 const authRoute = require('./routes/authRoute');
 const adminRoute = require('./routes/adminRoute');
 
-
 const app = express();
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 connectDB();
-
 
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 app.use(helmet());
 
 app.use(session({
@@ -31,7 +29,7 @@ app.use(session({
   saveUninitialized: false,
   store: MongoStore.create({ mongoUrl: process.env.MONGO_CONNECTION_URL }),
   cookie: {
-    maxAge: 1000 * 60 * 60 * 24, 
+    maxAge: 1000 * 60 * 60 * 24,
   },
 }));
 
@@ -39,6 +37,56 @@ app.use(morgan('dev'));
 
 app.use("/auth", authRoute);
 app.use("/data", adminRoute);
+
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage: storage,
+  fileFilter: (req, file, callback) => {
+    const allowedImageTypes = ['image/jpeg', 'image/png', 'image/gif']; 
+    if (allowedImageTypes.includes(file.mimetype)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Invalid file type. Please upload a valid image file.'));
+    }
+  }
+});
+
+app.post('/send-email', upload.fields([{ name: 'cv', maxCount: 1 }, { name: 'certificate', maxCount: 1 }]), async (req, res) => {
+  try {
+    const { fullname, email, link, phone } = req.body;
+
+    const cvBuffer = req.files['cv'][0].buffer;
+    const certificateBuffer = req.files['certificate'][0].buffer;
+
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD
+  }
+    });
+
+    const mailOptions = {
+      from: process.env.EMAIL,
+      to: email, 
+      subject: `Application from ${fullname}`,
+      text: `Full Name: ${fullname}\nEmail: ${email}\nLink: ${link}\nPhone: ${phone}`,
+      attachments: [
+        { filename: 'cv.pdf', content: cvBuffer, encoding: 'base64' },
+        { filename: 'certificate.png', content: certificateBuffer, encoding: 'base64' }
+      ]
+    };
+
+    // Send email
+    const info = await transporter.sendMail(mailOptions);
+    console.log('Email sent:', info.response);
+
+    res.status(200).send('Email sent successfully');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 app.get('/', (req, res) => {
   const filePath = path.join(__dirname, 'app', 'index.html');
